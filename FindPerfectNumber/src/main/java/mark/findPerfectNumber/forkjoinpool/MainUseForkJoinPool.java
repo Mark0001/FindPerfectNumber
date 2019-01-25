a28ce76eb27a0657eaeb9d2b53b133a2cc1d2c04
chihshengshih <chihsheng.shih@iisiroup.com>
2019-01-25 13:41:13 +0800

    新增 使用 forkjoinpool案例

---
+++ FindPerfectNumber/src/main/java/mark/findPerfectNumber/forkjoinpool/MainUseForkJoinPool.java
@@ -0,0 +1,34 @@
+package mark.findPerfectNumber.forkjoinpool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.ForkJoinPool;
+
+import org.apache.log4j.Logger;
+
+public class MainUseForkJoinPool {
+
+    final static Logger logger = Logger.getLogger(MainUseForkJoinPool.class);
+
+    private static int threadhold = 30;
+    private static int max = 8128;
+
+    public static void main(final String[] args) {
+
+        final ForkJoinPool pool = ForkJoinPool.commonPool();
+
+        logger.info("Parallelism  => " + pool.getParallelism());
+        final long start = System.nanoTime();
+        final List<Integer> ans = new ArrayList<>();
+
+        ans.addAll(pool.invoke(new WorkerForkJoin(1, max, threadhold)));
+        logger.info(ans);
+        pool.shutdown();
+
+        final long end = System.nanoTime();
+        final long timeInterval = end - start;
+        logger.info("結束應用");
+        logger.info("共用了" + timeInterval + "奈秒");
+        logger.info("共用了" + timeInterval / 1000000 + "毫秒");
+    }
+}
---
+++ FindPerfectNumber/src/main/java/mark/findPerfectNumber/forkjoinpool/WorkerForkJoin.java
@@ -0,0 +1,88 @@
+package mark.findPerfectNumber.forkjoinpool;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.RecursiveTask;
+
+import org.apache.log4j.Logger;
+
+public class WorkerForkJoin extends RecursiveTask<List<Integer>> {
+    private static final long serialVersionUID = 1L;
+
+    private int threadhold;
+
+    final static Logger logger = Logger.getLogger(WorkerForkJoin.class);
+
+    private int start;
+    private int end;
+
+    public WorkerForkJoin(final int start, final int end, final int threadhold) {
+        this.start = start;
+        this.end = end;
+        this.threadhold = threadhold;
+    }
+
+    @Override
+    protected List<Integer> compute() {
+
+        if (this.end - this.start >= this.threadhold) {
+            return this.findAnswer(this.start, this.end);
+        } else {
+            return divideWorks();
+        }
+    }
+
+    private List<Integer> divideWorks() {
+        final List<Integer> ans = new ArrayList<>();
+
+        final int middle = this.end / 2;
+
+        final WorkerForkJoin w1 = new WorkerForkJoin(this.start + 1, middle, this.threadhold);
+        final WorkerForkJoin w2 = new WorkerForkJoin(middle + 1, this.end, this.threadhold);
+
+        invokeAll(w1, w2);
+
+        ans.addAll(w1.join());
+        ans.addAll(w2.join());
+        return ans;
+    }
+
+    private List<Integer> findAnswer(final int start, final int end) {
+        final List<Integer> result = new ArrayList<>();
+        for (int i = start; i <= end; i++) {
+            //            logger.info(Thread.currentThread().getName() + "正在看" + i + "是不是完全數");
+            int temp = 1;
+            for (int count = 2; count < i; count++) {
+                if (count * count > i) {
+                    break;
+                }
+                if (i % count == 0) {
+                    temp = temp + count;
+                    final int temp1 = i / count;
+                    if (count != temp1) {
+                        temp = temp + temp1;
+                    }
+                } else {
+                    continue;
+                }
+                if (temp > i) {
+                    break;
+                }
+            }
+            if (temp == i) {
+                if (temp == 1) {
+                    continue;
+                }
+                logger.info(i + "是完美數字喔");
+                result.add(i);
+            }
+        }
+
+        try {
+            Thread.sleep(100);
+        } catch (final InterruptedException e) {
+            e.printStackTrace();
+        }
+        return result;
+    }
+}
